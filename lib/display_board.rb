7d42af10a2013a4aa4e51369c312feb346ca851f
gaccrux <venividivici47bc@hotmail.com>
2015-11-13 19:33:24 +0000

    nitrous-backup at Fri Nov 13 19:33:24 UTC 2015

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,30 @@
-# Define display_board that accepts a board and prints
-# out the current state.
+def display_board(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+
+def display_board(board = [" ", " ", " ", " ", "X", " ", " ", " ", " "])
+  puts "   |   |   "
+  puts "-----------"
+  puts "   | X |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+def display_board(board = ["O", " ", " ", " ", " ", " ", " ", " ", " "])
+  puts " O |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+
+def display_board(board = ["O", " ", " ", " ", "X", " ", " ", " ", " "])
+  puts " O |   |   "
+  puts "-----------"
+  puts "   | X |   "
+  puts "-----------"
+  puts "   |   |   "
+end
